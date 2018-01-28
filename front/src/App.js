da5b0e3865adc51e973322900d38d06b360174d9
Timur Bakibayev <timurbakibayev@gmail.com>
2018-01-28 13:02:49 +0600

    At least it works :)

--- front/src/App.js
+++ front/src/App.js
@@ -1,21 +1,30 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import MainComponent from './components/Main.js';
+import {HashRouter, Route, Switch} from 'react-router-dom';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    componentDidMount() {
+        document.title = "ClouDoc Service";
+    }
+
+    render() {
+        return (
+            <div className="App" style={{height: '100%'}}>
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <h1 className="App-title">ClouDoc</h1>
+                </header>
+                <HashRouter>
+                    <Switch>
+                        <Route render={(props) => (<MainComponent {...props}/>)}/>
+                    </Switch>
+                </HashRouter>
+
+            </div>
+        );
+    }
 }
 
 export default App;
--- front/src/index.js
+++ front/src/index.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import configureStore from './configureStore';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Root = ({ store }) => (
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
+
+
+const store = configureStore();
+ReactDOM.render(
+  <Root store={store} />,
+  document.getElementById('root')
+);
+document.getElementById('root').style.height = "100%";
 registerServiceWorker();
