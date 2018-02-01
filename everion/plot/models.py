a6034dece0f5182cf7e5a0aa4555ec000ef65ec6
Timur Bakibayev <timurbakibayev@gmail.com>
2018-02-01 09:53:49 +0600

    Added min and max fields for each type of readings

--- everion/plot/models.py
+++ everion/plot/models.py
@@ -5,6 +5,14 @@ class Patient(models.Model):
     name = models.CharField(max_length=200)
     birthday = models.DateField(null=True, blank=True)
     last_update = models.CharField(max_length=19, default="2018-01-20T01:00:00")
+    limits_hr_min = models.FloatField(default=70)
+    limits_hr_max = models.FloatField(default=110)
+    limits_spo2_min = models.FloatField(default=94)
+    limits_spo2_max = models.FloatField(default=100)
+    limits_bperf_min = models.FloatField(default=0.1)
+    limits_bperf_max = models.FloatField(default=0.75)
+    limits_rr_min = models.FloatField(default=10)
+    limits_rr_max = models.FloatField(default=50)
 
     def __str__(self):
         return self.name
