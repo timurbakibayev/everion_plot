ec022dc102b19f1f89f591483f2c07c6c6e71591
Timur Bakibayev <timurbakibayev@gmail.com>
2018-02-12 22:26:49 +0600

    database_settings.py

--- everion/everion/settings.py
+++ everion/everion/settings.py
@@ -77,13 +77,7 @@ WSGI_APPLICATION = 'everion.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
+from everion.database_settings import DATABASES
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
